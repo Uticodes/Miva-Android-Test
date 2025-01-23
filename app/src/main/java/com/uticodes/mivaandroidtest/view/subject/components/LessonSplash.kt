4874ce393ea24e49369c84c1a6b6f2e75b03430a
Uticodes <etim.utibe@gmail.com>
2025-01-23 02:29:58 +0100

    fix lint

--- app/src/main/java/com/uticodes/mivaandroidtest/view/subject/components/LessonSplash.kt
+++ app/src/main/java/com/uticodes/mivaandroidtest/view/subject/components/LessonSplash.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -30,7 +29,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.uticodes.mivaandroidtest.R
 import com.uticodes.mivaandroidtest.data.models.Subject
 import kotlinx.coroutines.delay
